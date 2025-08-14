e3c446b68520a0dc8435373462ac1c112b5e05fe
Makroboy <marko.soukka@gmail.com>
2025-08-14 20:12:02 +0300

    Create gigs.js
    Created file

---
+++ gigs.js
@@ -0,0 +1,5 @@
+window.gigs = [
+  { date: "2025-08-01", city: "Helsinki", venue: "Tavastia" },
+  { date: "2025-07-20", city: "Tampere", venue: "Hype Festival" },
+  { date: "2024-11-08", city: "Helsinki", venue: "Ääniwalli" }
+];
